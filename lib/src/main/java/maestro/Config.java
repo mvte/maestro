8cbb1e3f55b3a5fbdc7e55160d551f0e335ee4a7
mvte <janmarzan11@gmail.com>
2022-05-26 00:35:20 -0400

    heroku integration

--- lib/src/main/java/maestro/Config.java
+++ lib/src/main/java/maestro/Config.java
@@ -1,13 +1,9 @@
 package maestro;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Config {
-
-	private static final Dotenv dotenv = Dotenv.load();
 	
 	public static String get(String key) {
-		return dotenv.get(key.toUpperCase());
+		return System.getenv(key.toUpperCase());
 	}
 	
 }

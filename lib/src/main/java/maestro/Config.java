6510ec663ec36964d2639f342ed6e00abfeeabe3
mvte <janmarzan11@gmail.com>
2022-06-03 16:35:03 -0400

    update config.java

--- lib/src/main/java/maestro/Config.java
+++ lib/src/main/java/maestro/Config.java
@@ -6,11 +6,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class Config {
 	//when merging, change dotenv.get to System.getenv
 	
-	private static final Dotenv dotenv = Dotenv.load();
+//	private static final Dotenv dotenv = Dotenv.load();
 		//and get rid of this
 
 	public static String get(String key) {
-		return dotenv.get(key.toUpperCase());
-//		return System.getenv(key.toUpperCase());
+//		return dotenv.get(key.toUpperCase());
+		return System.getenv(key.toUpperCase());
 	}
 }

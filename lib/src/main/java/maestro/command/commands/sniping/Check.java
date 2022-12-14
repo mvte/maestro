657499a45d7194b6b99b53ab6570860567d688b8
mvte <janmarzan11@gmail.com>
2022-12-13 21:25:47 -0500

    command for user to see all their snipes

--- lib/src/main/java/maestro/command/commands/sniping/Check.java
+++ lib/src/main/java/maestro/command/commands/sniping/Check.java
@@ -1,6 +1,11 @@
 package maestro.command.commands.sniping;
 
+import maestro.Bot;
 import maestro.command.CommandInterface;
+import maestro.model.UserModel;
+import maestro.model.UserModelDatabase;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
@@ -8,6 +13,21 @@ import java.util.List;
 public class Check implements CommandInterface {
     @Override
     public void handle(MessageReceivedEvent event, List<String> args) {
+        UserModel user = UserModelDatabase.getInstance().getUser(event.getAuthor().getIdLong());
+        User jdaUser = Bot.bot.getUserById(user.getId());
+
+        EmbedBuilder eb = new EmbedBuilder()
+                .setTitle("your snipes")
+                .setThumbnail(jdaUser.getAvatarUrl())
+                .setFooter(jdaUser.getName());
+
+        for(maestro.sniper.Snipe s : user.getSnipes()) {
+            eb.addField(s.getItemName(), "[link](" + s.getUrl() + ")", false);
+        }
+
+        jdaUser.openPrivateChannel()
+                .flatMap(channel -> channel.sendMessageEmbeds(eb.build()))
+                .queue();
 
     }
 
@@ -18,6 +38,6 @@ public class Check implements CommandInterface {
 
     @Override
     public String getHelp(String prefix) {
-        return null;
+        return "gives list of snipes";
     }
 }

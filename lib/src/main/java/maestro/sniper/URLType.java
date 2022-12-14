915f8f1a31eb2d7a91f09360a1b0cd6f06c8c0aa
mvte <janmarzan11@gmail.com>
2022-12-13 21:37:32 -0500

    new check for rutgers url types

--- lib/src/main/java/maestro/sniper/URLType.java
+++ lib/src/main/java/maestro/sniper/URLType.java
@@ -4,10 +4,11 @@ public enum URLType {
 
     BEST_BUY ("https://www.bestbuy.com"),
     AMAZON ("https://www.amazon.com"),
-    GAMESTOP ("https://www.gamestop.com");
+    GAMESTOP ("https://www.gamestop.com"),
+    RUTGERS(" ");
 
-    private String prefix;
-    private URLType(String prefix) {
+    private final String prefix;
+    URLType(String prefix) {
         this.prefix = prefix;
     }
 
@@ -16,6 +17,10 @@ public enum URLType {
     }
 
     public static URLType getURLType(String check) {
+        if(check.matches("^\\d+$") && check.length() == 5) {
+            return RUTGERS;
+        }
+
         for(URLType url : URLType.values()) {
             if(check.startsWith(url.getPrefix())) {
                 return url;

8b5acabe6b0eb4bb1d315f47762c396d546fbd6a
mvte <janmarzan11@gmail.com>
2022-12-14 02:10:34 -0500

    begin implementation of course database

--- lib/src/main/java/maestro/model/RutgersCourseDatabase.java
+++ lib/src/main/java/maestro/model/RutgersCourseDatabase.java
@@ -1,6 +1,26 @@
 package maestro.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RutgersCourseDatabase {
+
+    private static final String coursesEndPoint = "https://sis.rutgers.edu/soc/api/courses.json?year=2023&term=1&campus=NB";
+    private static RutgersCourseDatabase INSTANCE;
+
+    private HashMap<String, RutgersCourse> courses;
+
+    private RutgersCourseDatabase() {
+        courses = new HashMap<>();
+    }
+
+    public static RutgersCourseDatabase getInstance() {
+        if(INSTANCE == null)
+            INSTANCE = new RutgersCourseDatabase();
+        return INSTANCE;
+    }
+
+
 
 
 

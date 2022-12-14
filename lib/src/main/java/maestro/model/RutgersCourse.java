b187c35da0aabdc4d9df031b533bef302c860139
mvte <janmarzan11@gmail.com>
2022-12-13 21:26:52 -0500

    framework for rutgers course db

---
+++ lib/src/main/java/maestro/model/RutgersCourse.java
@@ -0,0 +1,12 @@
+package maestro.model;
+
+import java.io.Serializable;
+
+public class RutgersCourse implements Serializable {
+
+    private int index;
+    private String title;
+    private String section;
+
+
+}

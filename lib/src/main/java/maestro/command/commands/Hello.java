184e6f16f9dc6467a1e80f0bd2ec173d68765eb0
mvte <janmarzan11@gmail.com>
2022-12-15 00:50:53 -0500

    hello now works when user dms bot

--- lib/src/main/java/maestro/command/commands/Hello.java
+++ lib/src/main/java/maestro/command/commands/Hello.java
@@ -3,6 +3,7 @@ package maestro.command.commands;
 import java.util.List;
 
 import maestro.command.CommandInterface;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -10,10 +11,17 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class Hello implements CommandInterface {
 
 	public void handle(MessageReceivedEvent event, List<String> args) {
-		TextChannel channel = event.getTextChannel();
+		MessageChannel channel = event.getChannel();
 		User user = event.getAuthor();
-		
-		channel.sendMessage("hello ***" + event.getGuild().getMember(user).getEffectiveName() + "***, how are you :D").queue();
+		String greet;
+		if(event.isFromGuild()) {
+			greet = "hello ***" + event.getGuild().getMember(user).getEffectiveName() + "***, how are you :D";
+		}
+		else {
+			greet = "hello ***" + user.getName() + "***, how are you :D";
+		}
+		channel.sendMessage(greet).queue();
+
 	}
 
 	public String getName() {

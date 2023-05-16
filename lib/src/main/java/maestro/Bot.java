e22f693716b31ea68fae2da1e4ac3ace057cbbd3
jan <57322982+mvte@users.noreply.github.com>
2023-05-16 03:26:00 -0400

    no more red text in console when someone leaves a vc

--- lib/src/main/java/maestro/Bot.java
+++ lib/src/main/java/maestro/Bot.java
@@ -57,7 +57,8 @@ public class Bot {
 		
 		audioManager.closeAudioConnection();
 		musicManager.audioPlayer.destroy();
-		musicManager.scheduler.future.cancel(false);
+		if(musicManager.scheduler.future != null)
+			musicManager.scheduler.future.cancel(false);
 	}
 	
 	

64dd527243f366d25db55f840ec286cb90d34db9
jan <57322982+mvte@users.noreply.github.com>
2023-05-21 19:13:15 -0400

    user interactions with blackjack buttons now registers them to db

--- lib/src/main/java/maestro/blackjack/interactions/JoinButton.java
+++ lib/src/main/java/maestro/blackjack/interactions/JoinButton.java
@@ -1,10 +1,15 @@
 package maestro.blackjack.interactions;
 
+import maestro.Bot;
 import maestro.blackjack.BlackjackManager;
 import maestro.blackjack.GuildGameManager;
 import maestro.blackjack.objects.Player;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class JoinButton implements Interaction {
 
@@ -17,8 +22,18 @@ public class JoinButton implements Interaction {
 			event.getHook().sendMessage("you cannot join a game that has already started").setEphemeral(true).queue();
 			return;
 		}
-		
-		Player player = new Player(1000, event.getUser());
+
+		if(manager.getConnection() == null) {
+			event.getHook().sendMessage("something went wrong joining the game, try again").setEphemeral(true).queue();
+			manager.retryConnection();
+			return;
+		}
+
+		Player player = BlackjackManager.getInstance().getPlayerFromDb(event.getUser(), manager.getConnection());
+		if(player == null) {
+			event.getHook().sendMessage("something went wrong joining the game, try again").setEphemeral(true).queue();
+			return;
+		}
 		
 		for(Player p : manager.getPlayers()) {
 			if(p.getUser().equals(event.getUser())) {
--- lib/src/main/java/maestro/blackjack/interactions/Start.java
+++ lib/src/main/java/maestro/blackjack/interactions/Start.java
@@ -1,11 +1,15 @@
 package maestro.blackjack.interactions;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.List;
 
+import maestro.Bot;
 import maestro.blackjack.BlackjackManager;
 import maestro.blackjack.GuildGameManager;
 import maestro.blackjack.objects.Player;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
@@ -26,11 +30,24 @@ public class Start implements Interaction {
 		.setThumbnail(channel.getJDA().getSelfUser().getAvatarUrl());
 		
 		GuildGameManager gameManager = BlackjackManager.getInstance().getGameManager(event.getGuild());
-		gameManager.nullGame();
-		gameManager.addPlayer(new Player(1000, event.getAuthor()));
-		
+		if(gameManager.started) {
+			channel.sendMessage("a game has already been started").queue();
+			return;
+		}
+
+		if(gameManager.getConnection() == null) {
+			channel.sendMessage("something went wrong starting the game, try again").queue();
+			gameManager.retryConnection();
+			return;
+		}
+		Player player = BlackjackManager.getInstance().getPlayerFromDb(event.getAuthor(), gameManager.getConnection());
+		if(player == null) {
+			channel.sendMessage("something went wrong starting the game, try again").queue();
+			return;
+		}
+		gameManager.addPlayer(player);
+
 		channel.sendMessageEmbeds(embed.build()).setActionRow(Button.success("blackjack:startbutton", "start"), Button.danger("blackjack:cancelbutton", "cancel"), Button.secondary("blackjack:join_button", "join")).queue();
-		return;
 	}
 	
 	@Override
